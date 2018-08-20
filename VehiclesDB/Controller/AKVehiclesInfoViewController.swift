6893fc3c88f323217ada7b9b3a82686c065c27ff
Andrei Kucherenko <aka2676@gmail.com>
2018-08-20 10:16:48 +0700

    added: view for detail information

---
+++ VehiclesDB/Controller/AKVehiclesInfoViewController.swift
@@ -0,0 +1,28 @@
+//
+//  AKVehiclesInfoViewController.swift
+//  VehiclesDB
+//
+//  Created by Andrei Kucherenko on 20/08/2018.
+//  Copyright © 2018 AK. All rights reserved.
+//
+
+import UIKit
+
+class AKVehiclesInfoViewController: UIViewController {
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+
+    }
+    
+    @IBAction func backButtonPressed(_ sender: AnyObject) {
+        self.dismiss(animated: true, completion: nil)
+    }
+
+
+}
